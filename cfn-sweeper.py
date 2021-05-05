3cc03111ad1317c9e85812df41a2b7ad41bfb276
UnKn0wN-Binary <71909554+UnKn0wN-Binary@users.noreply.github.com>
2021-05-05 19:24:20 -0400

    Added pypi build

---
+++ cfn-sweeper.py
@@ -0,0 +1,13 @@
+ascii_snek = """\
+    --..,_                     _,.--.
+       `'.'.                .'`__ o  `;__. Dakota is big Gey
+          '.'.            .'.'`  '---'`  `
+            '.`'--....--'`.'
+              `'--....--'`
+"""
+
+def main():
+    print(ascii_snek)
+    
+if __name__ == '__main__':
+    main()
