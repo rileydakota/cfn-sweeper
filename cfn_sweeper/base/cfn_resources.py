6ea7f2626b9b07878c25f4a09dc9f09513d881f5
rileydakota <73837129+rileydakota@users.noreply.github.com>
2021-05-06 09:40:24 -0400

    Initial structure (#1)
    * feat: initial commit
    
    * feat: add additional starting structure
    
    * chore: rename resource files
    
    * chore: add test structure
    
    * feat: add base resource file
    
    * Added pypi build
    
    * chore: fix setup.py
    
    * feat: inital scanning functions
    
    * test: more test case coverage
    
    * style: pep8
    
    Co-authored-by: UnKn0wN-Binary <71909554+UnKn0wN-Binary@users.noreply.github.com>

---
+++ cfn_sweeper/base/cfn_resources.py
@@ -0,0 +1,79 @@
+import boto3
+from botocore.config import Config
+
+cfn_config = Config(
+    retries={
+        'max_attempts': 10,
+        'mode': 'adaptive'
+    }
+)
+
+
+def load_cfn_resources(region: str) -> list:
+    """
+     Gets all the Cloudformation managed resources for the given AWS region
+
+     Parameters:
+         region (string): the AWS Region to scan
+
+     Returns:
+         An array of dict - containing the information of Cloudformation resources
+
+    """
+    cloudformation_client = boto3.client(
+        service_name='cloudformation', region_name=region, config=cfn_config)
+    stacks_in_account = []
+    stack_paginator = cloudformation_client.get_paginator('list_stacks')
+
+    stack_page_iterator = stack_paginator.paginate(
+        StackStatusFilter=['CREATE_COMPLETE', 'UPDATE_COMPLETE'])
+    for page in stack_page_iterator:
+        stacks_in_account.extend(page['StackSummaries'])
+    resource_paginator = cloudformation_client.get_paginator(
+        'list_stack_resources')
+    result = []
+    for stack in stacks_in_account:
+        resource_page_iterator = resource_paginator.paginate(
+            StackName=stack['StackName'])
+        for page in resource_page_iterator:
+            for resource in page['StackResourceSummaries']:
+                result.append(resource)
+    return result
+
+
+def get_all_cfn_resources_by_type(resource_array: list, resource_type: str) -> list:
+    """
+    Given a list of cloudformation stack resources, filters the resources by the specified type
+
+    Parameters:
+        resource_array (list): an Array of Cloudformation Stack Resources
+        resource_type (string): the Name of the Cloudformation Resource type to filter for - example: AWS::EC2::Instance
+
+    Returns:
+        An array of dict - containing the filtered Cloudformation resources
+    """
+    result = []
+
+    for resource in resource_array:
+        if resource['ResourceType'] == resource_type:
+            result.append(resource)
+    return result
+
+
+def is_managed_by_cloudformation(physical_resource_id: list, resource_array: list) -> bool:
+    """
+    Given a physical resource id and array of rources - returns if the resource is managed by cloudformation
+
+    Parameters:
+        physical_resource_id (string): The identifier of the resource - eg igw-09a7b4932e331edb2 or vpc-054a63a50efe678b3
+        resource_array (list): an array of cloudformation stack resources
+
+    Returns:
+        boolean - if the resource is found in the list of resource_array
+    """
+
+    for resource in resource_array:
+        if resource['PhysicalResourceId'] == physical_resource_id:
+            return True
+    else:
+        return False
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A tool for finding resources unmanaged by cloudformation'
+
+
+setup(
+    name="cfn-sweeper",
+    version=VERSION,
+    py_modules=["main"],
+    entry_points={
+        'console_scripts': [
+            'cfn_sweeper=cfn_sweeper.main:main',
+        ],
+    },
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'wheel'
+    ]
+    
+)
