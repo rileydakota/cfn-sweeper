3370de403010c70e3395f0f6e0b33a6e4da1ab1d
UnKn0wN-Binary <71909554+UnKn0wN-Binary@users.noreply.github.com>
2021-05-07 15:46:44 -0400

    added in arguments to main

--- cfn_sweeper/main.py
+++ cfn_sweeper/main.py
@@ -1,4 +1,6 @@
-ascii_snek = """\
+import argparse
+
+ascii_snek_big = """\
     --..,_                     _,.--.
        `'.'.                .'`__ o  `;__. 
           '.'.            .'.'`  '---'`  `
@@ -6,8 +8,45 @@ ascii_snek = """\
               `'--....--'`
 """
 
+ascii_snek_smoll = """\
+    --..,_               _,.--.
+       `'.'.           .'`__ o  `;__. 
+          '.'.       .'.'`  '---'`  `
+            '.`'..--'`
+              `'--....--'`
+"""
+
+
+def snek(n):
+    if n == 'smoll':
+        a = ascii_snek_smoll
+    elif n == 'big':
+        a = ascii_snek_big
+    else:
+        a = "No snek could be found for your argument"
+    return a
+
+
 def main():
-    print(ascii_snek)
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="Enter a name like smoll or big")
+    parser.add_argument('--outputDirectory',
+                        help='Path to the output that contains the resumes.',
+                        dest="output")
+
+    args = parser.parse_args()
+
+    result = snek(args.name)
+    output = args.output + 'snek.txt'
+
+    print("The name supplied was:" + " " + str(args.name) +
+          " " + "and here is the snek if found" + '\n')
+    print(result)
+
+    if args.output:
+        f = open(output, "a")
+        f.write(str(result))
+
+
 if __name__ == '__main__':
     main()
