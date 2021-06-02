eda5090529c994434bd2620313041be8c062e87d
rileydakota <73837129+rileydakota@users.noreply.github.com>
2021-06-02 13:13:55 -0400

    bug: fix s3 bucket lookups pulling buckets from all regions (#24)

--- cfn_sweeper/resources/AWS_S3_Bucket.py
+++ cfn_sweeper/resources/AWS_S3_Bucket.py
@@ -8,8 +8,15 @@ class resource(base_resource):
 
     def gather(self, region):
         results = []
-        s3_client = boto3.client('s3', region_name=region)
+        s3_client = boto3.client('s3', region_name=None)
         raw_response = s3_client.list_buckets()
         for bucket in raw_response['Buckets']:
-            results.append(bucket['Name'])
+            bucket_region = s3_client.get_bucket_location(Bucket=bucket['Name'])['LocationConstraint']
+            if region == 'us-east-1':
+                if bucket_region == None:
+                   results.append(bucket['Name']) 
+            elif bucket_region == region:
+                results.append(bucket['Name'])
+           
+
         return results
